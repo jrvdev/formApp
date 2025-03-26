9340e2c23a9d15d7bb691882c214c0e465b0452d
jrvdev <188245562+jrvdev@users.noreply.github.com>
2025-03-26 18:53:04 -0400

    feat: update,search,delete fun

--- app/src/main/java/com/darkbyte/formapp/MainActivity.kt
+++ app/src/main/java/com/darkbyte/formapp/MainActivity.kt
@@ -43,4 +43,60 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,"Los campos deben tener texto",Toast.LENGTH_LONG).show()
         }
     }
+
+    fun buscar(view: View){
+        val con = SQLite(this, "tienda", null, 1)
+        val baseDatos = con.writableDatabase
+        val codigo = txtCodigo?.text.toString()
+
+        if (codigo.isEmpty() == false){
+            val fila = baseDatos.rawQuery("select descripcion, precio from productos where codigo = $codigo", null)
+            if (fila.moveToFirst() == true){
+                txtDescripcion?.setText(fila.getString(0))
+                txtPrecio?.setText(fila.getString(1))
+                baseDatos.close()
+            }else  {
+                txtDescripcion?.setText("")
+                txtPrecio?.setText("")
+                Toast.makeText(this,"No se encontraron registros", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun eliminar(view: View){
+        val con = SQLite(this, "tienda", null, 1)
+        val baseDatos = con.writableDatabase
+        val codigo = txtCodigo?.text.toString()
+        if (codigo.isEmpty() == false){
+            val cant = baseDatos.delete("productos", "codigo='$codigo'", null)
+            if (cant > 0){
+                Toast.makeText(this, "El producto fue eliminado", Toast.LENGTH_LONG).show()
+            } else{
+                Toast.makeText(this, "El producto no fue encontrado", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun editar (view: View){
+        val con = SQLite(this, "tienda", null, 1)
+        val baseDatos = con.writableDatabase
+        val codigo = txtCodigo?.text.toString()
+        val descripcion = txtDescripcion?.text.toString()
+        val precio = txtPrecio?.text.toString()
+        if (!codigo.isEmpty() && !descripcion.isEmpty() && !precio.isEmpty()){
+            var registro = ContentValues()
+            registro.put("codigo", codigo)
+            registro.put("descripcion", descripcion)
+            registro.put("precio", precio)
+            var cant=baseDatos.update("productos", registro, "codigo='$codigo'", null)
+            if (cant>0){
+                Toast.makeText(this, "El registro se a editado exitosamente", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "El registro no fue encontrado", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, "Los campos no deben de estar vacios", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
